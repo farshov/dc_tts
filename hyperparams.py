7f5ef15901c543a35ebcb670218ddeb0b8c067c8
Медведев Игорь Сергеевич <medvedev.is@phystech.edu>
2020-08-13 15:48:21 +0300

    batch size

--- hyperparams.py
+++ hyperparams.py
@@ -48,5 +48,5 @@ class Hyperparams:
     lr = 0.001 # Initial learning rate.
     logdir = "logdir/minaev"
     sampledir = 'samples'
-    B = 24 # batch size
+    B = 16 # batch size
     num_iterations = 2000000
